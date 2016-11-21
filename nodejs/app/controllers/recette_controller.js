082b56a424bd92103949ef5baebb10e2043706a8
clejacquet <clem.jacquet@gmail.com>
2016-11-22 00:26:35 +0100

    recette model example modified

--- nodejs/app/controllers/recette_controller.js
+++ nodejs/app/controllers/recette_controller.js
@@ -3,11 +3,21 @@ var recette_model = require('../models/recette_model');
 module.exports = {
     /** VIEWS **/
     index_view: function (req, res) {
-        recette_model.get_from_id(0, function (err, recette) {
+        var id = req.params.id || 0;
+        recette_model.get_from_id(id, function (err, recette) {
             if (err) {
                 return res.status(500).json(err);
             }
 
+            if (recette == undefined) {
+                return res.status(404).render('error', {
+                    message: 'Not Found',
+                    error: {
+                        status: 404,
+                        stack: 'Recette ' + id + ' not found'
+                    }
+                });
+            }
             res.render('recette', recette);
         });
     },
--- nodejs/app/routes/recette_view.js
+++ nodejs/app/routes/recette_view.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var recette_controller = require('../controllers/recette_controller');
 
 router.get('/', recette_controller.index_view);
+router.get('/:id', recette_controller.index_view);
 
 module.exports = router;
