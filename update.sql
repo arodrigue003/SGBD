984ae9d7ea167a63202f32cec1f458107082e69a
clejacquet <clem.jacquet@gmail.com>
2016-11-24 11:04:42 +0100

    testdata1

---
+++ update.sql
@@ -0,0 +1,5 @@
+/* RECETTE */
+
+INSERT INTO recette(nom_recette, date_creation, temps_preparation, temps_cuisson, nombre_personnes, texte_preparation)
+VALUES ('Flan', NOW(), TIME '00:30:00', TIME '00:20:00', 4, 'Préparation du flan');
+
